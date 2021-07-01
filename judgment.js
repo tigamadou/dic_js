eb51bfaf1dc930941d61863a992e2964c015097d
Amadou <amadou140589@gmail.com>
2021-07-01 19:56:47 +0100

    get_pass_or_failure added to jugment

--- judgment.js
+++ judgment.js
@@ -1,15 +1,23 @@
-function get_achievement(x,y,z){
-    let sum = x+y+z,ans;
-    if(sum > 250){
-      ans =  'A';
-    }else if(sum >= 200){
+function get_achievement(x, y, z) {
+    let sum = x + y + z, ans;
+    if (sum > 250) {
+        ans = 'A';
+    } else if (sum >= 200) {
         ans = 'B'
-    }else if(sum >= 100){
+    } else if (sum >= 100) {
         ans = 'C'
-    }else{
+    } else {
         ans = 'D'
     }
     return ans;
-  }
+}
 
-  console.log(get_achievement(60,100,40));
+function get_pass_or_failure(x, y, z) {
+    if (x >= 60 && y >= 60 && z >= 60) {
+        return "Pass";
+    }else{
+        return "Fail";
+    }
+    //… Continued
+}
+console.log(get_pass_or_failure(60, 100, 60));
